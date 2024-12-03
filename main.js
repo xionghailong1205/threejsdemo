16a86862719b4e1d079c57df9e5d146336ce6d8c
熊海龙 <xionghailongk@gmail.com>
2024-12-03 22:02:24 +0800

    进度保存

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
 import * as THREE from "three";
+import "./style.css";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { PLYLoader } from "three/examples/jsm/Addons.js";
 import Stats from "three/addons/libs/stats.module.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
-const width = window.innerWidth,
-  height = window.innerHeight;
-
-// init
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 1000);
 const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-const controls = new OrbitControls(camera, renderer.domElement);
+const gui = new GUI();
 
-const ambientLight = new THREE.AmbientLight("#606008", 1);
-scene.add(ambientLight);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-controls.enableDamping = true;
+const data = {
+  lightColor: 0xffffff,
+  color: 0x00ff00,
+};
 
-let mesh;
+camera.position.z = 300;
 
+// const axesHelper = new THREE.AxesHelper(150);
+// scene.add(axesHelper);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// 设置光源
+const pointLight1 = new THREE.PointLight(data.lightColor, 20);
+pointLight1.position.set(100, 0, 100);
+pointLight1.decay = 0.0;
+scene.add(pointLight1);
+
+const pointLight2 = new THREE.PointLight(data.lightColor, 20);
+pointLight2.position.set(-100, 0, -100);
+pointLight2.decay = 0.0;
+scene.add(pointLight2);
+
+// 光源相关控制器
+// const pointLightFolder = gui.addFolder("Pointlight");
+// pointLightFolder.add(pointLight, "visible");
+// pointLightFolder.addColor(data, "lightColor").onChange(function () {
+//   directionalLight.color.set(data.lightColor);
+// });
+// pointLightFolder.add(pointLight, "intensity", 0, Math.PI * 10);
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight1);
+// pointLightHelper.visible = true;
+// scene.add(pointLightHelper);
+
+// 当窗口大小变化时, 重绘窗口
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+new OrbitControls(camera, renderer.domElement);
+
+let segResultModel;
+let segResultMaterial;
+
+// 通过 PLY 加载器加载模型
 const loader = new PLYLoader();
 loader.load(
   "/model/test.ply",
@@ -26,24 +72,19 @@ loader.load(
     console.log(geometry);
     geometry.computeVertexNormals();
     geometry.computeBoundingBox();
+    geometry.center();
 
-    // 计算 AABB box 的中心
-    const { max, min } = geometry.boundingBox;
-    const modelCenter = [
-      (max.x + min.x) / 2,
-      (max.y + min.y) / 2,
-      (max.z + min.z) / 2,
-    ];
+    segResultMaterial = new THREE.MeshPhongMaterial({
+      color: data.color,
+      flatShading: true,
+    });
 
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const test = new THREE.BoxGeometry(20, 20, 20);
-    mesh = new THREE.Mesh(geometry, material);
+    segResultMaterial.color.set(0x297a29);
 
-    camera.lookAt(modelCenter);
-    camera.position.set(modelCenter[0], modelCenter[1] + 100, modelCenter[2]);
+    segResultModel = new THREE.Mesh(geometry, segResultMaterial);
+
     renderer.render(scene, camera);
-
-    scene.add(mesh);
+    scene.add(segResultModel);
   },
   (xhr) => {
     // console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
@@ -56,20 +97,18 @@ loader.load(
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
-renderer.setSize(width, height);
-renderer.setAnimationLoop(animate);
+const modelFolder = gui.addFolder("模型");
+modelFolder.addColor(data, "color").onChange(function () {
+  segResultMaterial.color.set(data.color);
+});
+modelFolder.open();
 
-document.body.appendChild(renderer.domElement);
-
-// animation
-
-function animate(time) {
-  // if (mesh) {
-  //   mesh.rotation.x = time / 2000;
-  //   mesh.rotation.y = time / 1000;
-  // }
-
-  // console.log("代码执行");
+function animate() {
+  requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
+
+  stats.update();
 }
+
+animate();
